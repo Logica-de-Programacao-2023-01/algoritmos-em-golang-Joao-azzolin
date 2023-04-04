7690557582a3c0a40d20ffe0e729974e41c6bb73
João Rafael Azzolin Vieira <joao.azzolin@dinfor.uniceub>
2023-04-04 09:26:16 -0300

    sequenciais2

---
+++ seq2..go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int
+	fmt.Print("Insira um número:")
+	fmt.Scan(&x)
+	fmt.Println("Seu dobro é:", 2*x, "Seu triplo é:", 3*x, "Seu quádruplo é:", 4*x)
+
+}
